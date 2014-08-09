daeab6d8502a734c358d237ca82d374b540850d1
sidk <krishnan.sid@gmail.com>
2014-08-09 15:27:21 -0400

    updated question methods to deal with cycling and single quesiton situations

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,7 +10,7 @@ class Question < ActiveRecord::Base
   
   def self.next_serious_question_id(question_id)
     begin
-      where('user_id IS NULL').where(question_type: 'serious').where('id > ?', question_id).order("id ASC").first.id
+      first_sorted_question("serious", question_id, "ASC").id
     rescue
       where('user_id IS NULL').where(question_type: 'serious').first.id
     end
@@ -18,26 +18,26 @@ class Question < ActiveRecord::Base
 
   def self.next_fun_question_id(question_id)
     begin
-      where('user_id IS NULL').where(question_type: 'fun').where('id > ?', question_id).order("id ASC").first.id
+      first_sorted_question("fun", question_id, "ASC").id
     rescue
       where('user_id IS NULL').where(question_type: 'fun').first.id
     end
   end
   
   def self.previous_fun_question(question_id)
-    begin
-      where('user_id IS NULL').where(question_type: 'fun').where('id < ?', question_id).order("id DESC").first
-    rescue
-      where('user_id IS NULL').where(question_type: 'fun').last
-    end
+    question = first_sorted_question("fun", question_id, "DESC")
+    return question unless question.nil?
+    where('user_id IS NULL').where(question_type: 'fun').last
   end
 
   def self.previous_serious_question(question_id)
-    begin
-      where('user_id IS NULL').where(question_type: 'serious').where('id < ?', question_id).order("id DESC").first
-    rescue
-      where('user_id IS NULL').where(question_type: 'serious').last
-    end
+    question = first_sorted_question("serious", question_id, "DESC")
+    return question unless question.nil?
+    where('user_id IS NULL').where(question_type: 'serious').last
+  end
+  
+  def self.first_sorted_question(question_type, question_id, order)
+    where('user_id IS NULL').where(question_type: question_type).where('id < ?', question_id).order("id #{order}").first
   end
   
   
